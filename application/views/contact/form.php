0d6ba108e7681d00e4071820b2869d03bd5519f4
Dannypein <danieldolores117@gmail.com>
2015-08-29 11:28:26 -0500

    eliminar a un miembro del grupo
    se elimino a un miembro del grupo de manera temporal

--- application/views/contact/form.php
+++ application/views/contact/form.php
@@ -40,40 +40,42 @@
         desea adquirirla, o si tiene alguna duda que desea resolver,
         estamos a sus órdenes en los siguientes teléfonos:
       </p>
-    <br>
-    <div class="col-sm-12 col-md-6">
-      <img src="<?=img_url().'fotop4.jpg'?>" style="width: 105px; height: 135px; border: 2px solid gray;">
-      <p>Dina Armas</p>
-      <p>ventasmzo02@gmail.com</p>
-    
-      <img src="<?=img_url().'fotop1.jpg'?>" style="width: 105px; height: 135px; border: 2px solid gray;">
-      <p>Angela Rocha</p>
-      <p>belenizhi19@gmail.com</p>
-    </div>
-    <div class="col-sm-12 col-md-6">
-      <img src="<?=img_url().'fotop3.jpg'?>" style="width: 105px; height: 135px; border: 2px solid gray;">
-      <p>Rosa Maria Rincon</p>
-      <p>rosyma58@hotmail.com</p>
-    
-      <img src="<?=img_url().'fotop2.jpg'?>" style="width: 105px; height: 135px; border: 2px solid gray;">
-      <p>Jose Luis Aguilar</p>
-      <p>joseluis57ventas@gmail.com</p>
-    </div>
-    <br>
-      <p style="font-size: 2em"><b>Telefono y horario de oficina</b></p>
-        <div class="phone">
-        <i class="glyphicon glyphicon-phone"></i>
-          <span> 688-1194 </span>
-        </div>
-         <p style="font-size: 1.5em">Horario de: 10:00 AM a 14:00 PM</p>
-         <p>Otros Telefonos</p>
-        <div >
+      <br>
+      <div class="col-sm-12 col-md-6">
+        <img src="<?=img_url().'fotop4.jpg'?>" style="width: 105px; height: 135px; border: 2px solid gray;">
+        <p>Dina Armas</p>
+        <p>ventasmzo02@gmail.com</p>
+      
+        <img src="<?=img_url().'fotop1.jpg'?>" style="width: 105px; height: 135px; border: 2px solid gray;">
+        <p>Angela Rocha</p>
+        <p>belenizhi19@gmail.com</p>
+      </div>
+      <div class="col-sm-12 col-md-6">
+        <!--<img src="<?=img_url().'fotop3.jpg'?>" style="width: 105px; height: 135px; border: 2px solid gray;">
+        <p>Rosa Maria Rincon</p>
+        <p>rosyma58@hotmail.com</p>-->
+      
+        <img src="<?=img_url().'fotop2.jpg'?>" style="width: 105px; height: 135px; border: 2px solid gray;">
+        <p>Jose Luis Aguilar</p>
+        <p>joseluis57ventas@gmail.com</p>
+      </div>
+      <br>
+      <div  class="col-sm-12 col-md-6">
+        <p style="font-size: 2em"><b>Telefono y horario de oficina</b></p>
+          <div class="phone">
           <i class="glyphicon glyphicon-phone"></i>
-          <span>314 142-8513</span>
-        </div>
-        <div >
-        <i class="glyphicon glyphicon-phone"></i>
-          <span>314 109-3511</span>
+            <span> 688-1194 </span>
+          </div>
+           <p style="font-size: 1.5em">Horario de: 10:00 AM a 14:00 PM</p>
+           <p>Otros Telefonos</p>
+          <div >
+            <i class="glyphicon glyphicon-phone"></i>
+            <span>314 142-8513</span>
+          </div>
+          <div >
+          <i class="glyphicon glyphicon-phone"></i>
+            <span>314 109-3511</span>
+          </div>
         </div>
     </div>
 
