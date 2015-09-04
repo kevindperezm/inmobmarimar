f1d46c21f92135b42907a9078a7a8885f1c493a5
Dannypein <danieldolores117@gmail.com>
2015-09-04 13:26:00 -0500

    crear un view de zonas
    es la vista donde viene el mapa de las zonas

---
+++ application/views/areas/index.php
@@ -0,0 +1,39 @@
+<?= css('properties.css') ?>
+
+<div class="container">
+  <div class="row">
+    <div class="col-md-2">
+      <ul>
+        <li class="list-group-item"><p>1.- Manzanillo Centro</p></li>
+        <li class="list-group-item"><p>2.- San Pedrito</p></li>
+        <li class="list-group-item"><p>3.- Tapeixtles</p></li>
+        <li class="list-group-item"><p>4..- Valle de las garzas</p></li>
+        <li class="list-group-item"><p>5.- Las brisas</p></li>
+        <li class="list-group-item"><p>6.- Salagua</p></li>
+        <li class="list-group-item"><p>7.- Las Hadas</p></li>
+        <li class="list-group-item"><p>8.- La punta</p></li>
+        <li class="list-group-item"><p>9.- Santiago</p></li>
+        <li class="list-group-item"><p>10.- Miramar</p></li>
+        <li class="list-group-item"><p>11.- Club Santiago</p></li>
+        <li class="list-group-item"><p>12.- Vida del mar</p></li>
+      </ul>
+    </div>
+    <div class="col-md-10">
+      <img src="img/mapa2.JPG" usemap="#Map" style="width: 820px; height: 455px;">
+        <map name="MAP">
+          <area shape="circle" coords="570,435,20" href="" alt="Manzanillo Centro">
+          <area shape="circle" coords="625,400,15" href="" alt="San Pedrito">
+          <area shape="circle" coords="704,352,15" href="" alt="Tapeixtles">
+          <area shape="circle" coords="620,322,17" href="" alt="Valle de las Garzas">
+          <area shape="circle" coords="625,215,28" href="" alt="Las Brisas">
+          <area shape="circle" coords="520,160,22" href="" alt="Salagua">
+          <area shape="circle" coords="438,160,18" href="" alt="Las Hadas">
+          <area shape="circle" coords="410,222,18" href="" alt="La punta">
+          <area shape="circle" coords="408,100,15" href="" alt="Santiago">
+          <area shape="circle" coords="300,100,18" href="" alt="Miramar">
+          <area shape="circle" coords="212,118,19" href="" alt="Club Santiago">
+          <area shape="circle" coords="160,170,18" href="" alt="Vida del mar">
+        </map>
+    </div>
+  </div>
+</div>
