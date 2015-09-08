ae61495832a3911ca2189186694c3e5b0db5a5a4
Kevin Perez <kevindperezm@gmail.com>
2015-09-08 11:31:52 -0500

    añadir rutas para ver áreas
    Estas rutas se usarán para mostrar el mapa de áreas y mostrar las propiedades de
    cada área.

--- application/config/routes.php
+++ application/config/routes.php
@@ -12,5 +12,8 @@ $route["($property_kinds)/($sell_modes)"]               = 'properties/index/$1/$
 $route['propiedades/(:num)']                            = 'properties/show/$1';
 $route['propiedades/nuevos-fraccionamientos']           = 'properties/new_suburbs';
 
+$route['areas']                                         = 'areas/index';
+$route['areas/(:num)']                                  = 'areas/show/$1';
+
 $route['contacto']        = 'contact/show_form';
 $route['contacto/enviar'] = 'contact/send_mail';
