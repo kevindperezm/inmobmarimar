1427223d6b4244d619fe25bbaccbdc87bd673bc5
Dannypein <luigidanny@hotmail.com>
2015-09-16 19:52:53 -0500

    corregir ruta de imagen de areas

--- application/views/areas/index.php
+++ application/views/areas/index.php
@@ -11,7 +11,7 @@
     </div>
 
     <div class="col-md-10">
-      <img src="assets/img/mapa-areas.jpg" usemap="#map" style="width: 820px; height: 455px;">
+      <img src="<?= img_url() . 'mapa-areas.jpg' ?>" usemap="#map" style="width: 820px; height: 455px;">
         <map name="map">
           <?php foreach ($areas as $area) : ?>
             <?= shape_for_area($area) ?>
