7eb4bddc4dc409290eae529aa2c9a166d26f20a0
Kevin Perez <kevindperezm@gmail.com>
2015-10-26 20:29:03 -0600

    refactorizar menú de navegación del sitio
    Esto mueve el html del navbar del sitio a su propia view.

--- application/views/layouts/master.php
+++ application/views/layouts/master.php
@@ -39,25 +39,7 @@
               </div>
               <div class="collapse navbar-collapse dropdown" id="bs-example-navbar-collapse-1">
                 <ul class="nav navbar-nav">
-                  <li><a href="<?=site_url()?>">Inicio</a></li>
-                  <li><a href="<?=site_url('contacto')?>">Contacto</a></li>
-                  <li class="dropdown">
-                    <a href="#"class="dropdown-toggle" data-toggle="dropdown">Propiedades <span class="caret"></span></a>
-                    <ul class="dropdown-menu">
-                      <li><a href="<?=site_url('propiedades/ventas')?>">En venta</a></li>
-                      <li><a href="<?=site_url('propiedades/rentas')?>">En renta</a></li>
-                      <li><a href="<?=site_url('areas')?>">Por áreas</a></li>
-                    </ul>
-                  </li>
-                  <li class="dropdown">
-                    <a href="#"class="dropdown-toggle" data-toggle="dropdown">Terrenos <span class="caret"></span></a>
-                    <ul class="dropdown-menu">
-                      <li><a href="<?=site_url('terrenos/ventas')?>">En venta</a></li>
-                      <li><a href="<?=site_url('terrenos/rentas')?>">En renta</a></li>
-                      <li><a href="<?=site_url('areas')?>">Por áreas</a></li>
-                    </ul>
-                  </li>
-                  <li><a href="<?=site_url('propiedades/nuevos-fraccionamientos')?>">Fraccionamientos nuevos</a></li>
+                  <?php $this->load->view('layouts/menu') ?>
                 </ul>
                 <?= form_open(site_url('propiedades/buscar'), 'class="navbar-form navbar-right"') ?>
                   <div class="input-group">
---
+++ application/views/layouts/menu.php
@@ -0,0 +1,19 @@
+<li><a href="<?=site_url()?>">Inicio</a></li>
+<li><a href="<?=site_url('contacto')?>">Contacto</a></li>
+<li class="dropdown">
+  <a href="#"class="dropdown-toggle" data-toggle="dropdown">Propiedades <span class="caret"></span></a>
+  <ul class="dropdown-menu">
+    <li><a href="<?=site_url('propiedades/ventas')?>">En venta</a></li>
+    <li><a href="<?=site_url('propiedades/rentas')?>">En renta</a></li>
+    <li><a href="<?=site_url('areas')?>">Por áreas</a></li>
+  </ul>
+</li>
+<li class="dropdown">
+  <a href="#"class="dropdown-toggle" data-toggle="dropdown">Terrenos <span class="caret"></span></a>
+  <ul class="dropdown-menu">
+    <li><a href="<?=site_url('terrenos/ventas')?>">En venta</a></li>
+    <li><a href="<?=site_url('terrenos/rentas')?>">En renta</a></li>
+    <li><a href="<?=site_url('areas')?>">Por áreas</a></li>
+  </ul>
+</li>
+<li><a href="<?=site_url('propiedades/nuevos-fraccionamientos')?>">Fraccionamientos nuevos</a></li>
