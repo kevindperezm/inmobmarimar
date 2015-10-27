3c5ec18e92dcca901d581fc17cbbedf643f5b984
Kevin Perez <kevindperezm@gmail.com>
2015-10-26 20:31:54 -0600

    refactorizar form de búsqueda en navbar
    Esto mueve el html del formulario de búsqueda incrustado en el navbar a su
    propia view.

--- application/views/layouts/master.php
+++ application/views/layouts/master.php
@@ -41,16 +41,7 @@
                 <ul class="nav navbar-nav">
                   <?php $this->load->view('layouts/menu') ?>
                 </ul>
-                <?= form_open(site_url('propiedades/buscar'), 'class="navbar-form navbar-right"') ?>
-                  <div class="input-group">
-                    <?= form_input('keywords', set_value('keywords', null), 'class="form-control" placeholder="Buscar propiedades"') ?>
-                    <span class="input-group-btn">
-                      <button class="btn btn-default" type="submit">
-                        <i class="glyphicon glyphicon-search"></i>
-                      </button>
-                    </span>
-                  </div>
-                <?= form_close() ?>
+                <?php $this->load->view('layouts/search') ?>
               </div>
 
             </div>
